abbd909d4ccc412c9071034c49dfed851c17035a
Claudia <caucrah@gmail.com>
2022-02-27 00:44:08 +0100

    Adding hero effect

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,25 @@
 import React, { useRef, useEffect } from "react";
 import tommyHeader from "../../assets/images/tommy-header.jpg";
 import "./Header.css";
-
 import CSSRulePlugin from "gsap/CSSRulePlugin";
+import { gsap } from "gsap";
+import { Power2 } from "gsap/all";
 
 const Header = () => {
+  // eslint-disable-next-line no-unused-vars
   let imageContainer = useRef(null);
+  // eslint-disable-next-line no-unused-vars
   let image = useRef(null);
   let imageReveal = CSSRulePlugin.getRule(".img-container:after");
 
+  let tl = gsap.timeline();
+
   useEffect(() => {
-    console.log(imageContainer, image, imageReveal);
+    tl.to(".img-container", 1.2, { css: { visibility: "visible" } }).to(
+      imageReveal,
+      1,
+      { width: "0%", ease: Power2.easeInOut },
+    );
   });
 
   return (
@@ -26,6 +35,14 @@ const Header = () => {
           />
         </div>
       </section>
+      <div className="big-text">
+        <h1>
+          <span className="hide-text love-text">LOVE.</span>
+        </h1>
+        <h1>
+          <span className="hide-text la-text">L.A.</span>
+        </h1>
+      </div>
     </div>
   );
 };
